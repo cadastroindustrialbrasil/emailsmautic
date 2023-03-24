687d024faef6412e24ee929f7e64c5a8208de658
cadastroindustrialbrasil <cadastroindustrialbrasil@gmail.com>
2023-03-23 23:31:33 -0300

    deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-    var sequelize = new Sequelize('eduard72_consultagoogle', 'eduard72_felipe', 'oQnD~rzZWG&9', {
+var sequelize = new Sequelize('eduard72_consultagoogle', 'eduard72_felipe', 'oQnD~rzZWG&9', {
     host: 'sh-pro20.hostgator.com.br',
     dialect: "mysql",
     define: {
@@ -10,19 +10,16 @@ import Sequelize from 'sequelize';
     logging: false
 });
 
-
-
 sequelize.authenticate().then(() => { }).catch(err => {
     console.error('Erro ao conectar a banco de dados: ', err);
 });
 
-async function app(){
+async function app() {
 
- await sequelize.query("DELETE from emails where id not in ( SELECT * FROM(select min(id) from emails group by email) AS temp_tab)");
-        await sequelize.query('DELETE FROM emails WHERE email like "%1%" || email like "%2%" || email like "%3%" ||email like "%4%" || email like "%5%" || email like "%6%" || email like "%7%" || email like "%8%" || email like "%9%"');
+    await sequelize.query("DELETE from emails where id not in ( SELECT * FROM(select min(id) from emails group by email) AS temp_tab)");
+    await sequelize.query('DELETE FROM emails WHERE email like "%1%" || email like "%2%" || email like "%3%" ||email like "%4%" || email like "%5%" || email like "%6%" || email like "%7%" || email like "%8%" || email like "%9%"');
 
-
-         var getEmail = await sequelize.query("SELECT * FROM `emails`");
+    var getEmail = await sequelize.query("SELECT * FROM `emails`");
 
     console.log(getEmail)
 }
