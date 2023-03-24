5f9be021a72e0dc3a606a1a82fd149739068c727
cadastroindustrialbrasil <cadastroindustrialbrasil@gmail.com>
2023-03-23 23:25:51 -0300

    deploy

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const Sequelize = require('sequelize');
-const {
-    QueryTypes
-} = require('sequelize');
-const axios = require('axios')
+import Sequelize from 'sequelize';
 
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-
- const sequelize = new Sequelize('eduard72_consultagoogle', 'eduard72_felipe', 'oQnD~rzZWG&9', {
+    var sequelize = new Sequelize('eduard72_consultagoogle', 'eduard72_felipe', 'oQnD~rzZWG&9', {
     host: 'sh-pro20.hostgator.com.br',
     dialect: "mysql",
     define: {
@@ -18,54 +11,19 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 });
 
 
+
 sequelize.authenticate().then(() => { }).catch(err => {
     console.error('Erro ao conectar a banco de dados: ', err);
 });
 
 async function app(){
 
-await sequelize.query("DELETE from emails where id not in ( SELECT * FROM(select min(id) from emails group by email) AS temp_tab)");
-await sequelize.query('DELETE FROM emails WHERE email like "%1%" || email like "%2%" || email like "%3%" ||email like "%4%" || email like "%5%" || email like "%6%" || email like "%7%" || email like "%8%" || email like "%9%"');
+ await sequelize.query("DELETE from emails where id not in ( SELECT * FROM(select min(id) from emails group by email) AS temp_tab)");
+        await sequelize.query('DELETE FROM emails WHERE email like "%1%" || email like "%2%" || email like "%3%" ||email like "%4%" || email like "%5%" || email like "%6%" || email like "%7%" || email like "%8%" || email like "%9%"');
 
 
-var emails = await sequelize.query("SELECT * FROM `emails`");
+         var getEmail = await sequelize.query("SELECT * FROM `emails`");
 
-emails.forEach(async function(email) {
-    
-    var name = email[0].email
-  
-    name = name.split("@")
-    name = name[0]
-
-    email = email[0]
-
-    var arrayFields = {
-        "firstname": name,
-        "email": email.email,
-        "state":email.estado,
-        "tags":[email.categoria,email.estado,"Google"]
-        }
-
-    var optionsMautic = {
-        method: 'post',
-        headers: {
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic Y2FkYXN0cm9icjpXZWJtYXN0ZXIxMjMxKg==',
-            'Cookie': 'mautic_device_id=1ptosd2ouwqe16617j5psrrn0'
-        },
-        data: JSON.stringify(arrayFields)
-    }
-
-    await axios.post('https://editoraeuro.com.br/api/contacts/new', optionsMautic)
-    .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-        .catch(err => console.log(err))
-
-        delay(3000)
-    
-})
+    console.log(getEmail)
 }
 app()
